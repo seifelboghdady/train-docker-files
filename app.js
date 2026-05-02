fecf690d0e5238dd0aec96e69847455e8c195b38
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-05-02 23:08:13 +0300

    Fix: repaire some issues

--- app.js
+++ app.js
@@ -3,40 +3,48 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
 
-client.on('error',(err)=>{console.log('Redis Client Error',err);});
-client.on('connect',()=>{console.log('Redis Connected ...');});
+// Redis
+const HOST_REDIS = 'redis';
+const PORT_REDIS = 6379;
 
+const client = redis.createClient({
+    url: `redis://${HOST_REDIS}:${PORT_REDIS}`
+});
+
+client.on('error', (err) => {
+    console.log('Redis Client Error', err);
+});
+
+client.on('connect', () => {
+    console.log('Redis Connected ...');
+});
+
+(async () => {
+    await client.connect();
+})();
+
+
+
+// Mongo
 const user = 'root';
 const password = 'example';
 const port = 27017;
-// const host = '172.18.0.3';
 const host = 'mongo';
+
 const URI = `mongodb://${user}:${password}@${host}:${port}`;
+
 mongoose.connect(URI)
-  .then(() => console.log('Connected!')).catch((err)=> console.log(err));
+    .then(() => console.log('Mongo Connected!'))
+    .catch((err) => console.log(err));
 
-app.get('/',(req, res)=>{
-    res.send(`
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <h1> Hello seif.....,</h1><br>
-    <h2>Text input fields</h2>
-
-    <form>
-    <label for="fname">Firs tname:</label><br>
-    <input type="text" id="fname" name="fname" value="seif"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="ashraf">
-    </form>
-    <p>Note that the form itself is not visible.</p>
-    <p>Also note that the default width of text input fields is 20 characters.</p>
-    </body>
-    </html>`);
+// Route
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Seif 🚀</h1>');
 });
 
-app.listen(4000, ()=>{
-    console.log('server run in localhost:4000');
-})
+
+// Server
+app.listen(4000, () => {
+    console.log('server running on port 4000');
+});
